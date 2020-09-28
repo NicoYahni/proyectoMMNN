cbe343661f576d7910266f946d6a6a29298e52cf
Marcos <hoggm@udesa.edu.ar>
2020-09-27 23:24:36 -0300

    localhost solo te lleva a home. cambio en app.js

--- proyectoMMN/app.js
+++ proyectoMMN/app.js
@@ -10,7 +10,7 @@ var usersRouter = require('./routes/users');
 var app = express();
 //Aca van nuestras rutas//
 let rutaHome = require ("./routes/home");
-app.use ("/home" , rutaHome);
+app.use ("/" , rutaHome);
 
 let rutaAgregarPost = require ("./routes/agregarPost");
 app.use ("/agregarPost", rutaAgregarPost);
