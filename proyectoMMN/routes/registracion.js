4270342edcd43edb8453b7b58c4f05336233c75d
Marcos <hoggm@udesa.edu.ar>
2020-11-06 17:41:21 -0300

    cambio en registracion

--- proyectoMMN/routes/registracion.js
+++ proyectoMMN/routes/registracion.js
@@ -2,6 +2,7 @@ var express = require('express');
 var router = express.Router();
 var controladorRegistracion = require ("../controllers/controladorRegistracion");
 
+
 router.get ("/", controladorRegistracion.registracion)
 
 
--- proyectoMMN/routes/users.js
+++ proyectoMMN/routes/users.js
@@ -1,11 +1,10 @@
 var express = require('express');
 var router = express.Router();
-let controladorRegistracion = require("../controllers/controladorRegistracion")
+
 /* GET users listing. */
 router.get('/', function(req, res, next) {
   res.send('respond with a resource');
 });
 
-router.get("/register", controladorRegistracion.registracion)
-router.post("/register"), controladorRegistracion.storeUser
+
 module.exports = router;
