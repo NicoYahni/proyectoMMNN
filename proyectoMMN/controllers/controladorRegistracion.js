1c18d90042024ba5d14b31acdca1981b9780572f
Marcos <hoggm@udesa.edu.ar>
2020-11-06 17:17:46 -0300

    registracion

--- proyectoMMN/controllers/controladorRegistracion.js
+++ proyectoMMN/controllers/controladorRegistracion.js
@@ -1,8 +1,28 @@
+let db = require("../db/models/index")
+
 let rutaRegistracion = {
 
     registracion: function(req, res) {
-        res.render("registracion", )   
+        res.render("registracion")   
+    },
+    storeUser: function(req, res) {
+        let nombre = req.body.nombreUsuario
+        let mail = req.body.emailUsuario 
+        let password = req.body.passwordUsuario
+
+        let usuario = {
+            name : nombre,
+            password : password,
+            email : mail
+        }
+                
+        db.Usuarios.create(usuario)
+        .then(function() {
+        res.redirect("/home")
+        })
     }
+   
+
 }
 
 
