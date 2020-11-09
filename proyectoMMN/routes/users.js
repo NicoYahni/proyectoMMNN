4fd83540272c65dc4d6c143b99fe0984729bc8ca
Marcos <hoggm@udesa.edu.ar>
2020-11-09 11:04:43 -0300

    intentando resolver error terminal controladorRegi

--- proyectoMMN/routes/users.js
+++ proyectoMMN/routes/users.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var router = express.Router();
+var controladorRegistracion = require ("../controllers/controladorRegistracion");
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
