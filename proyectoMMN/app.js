c425998f6b378bdd00f5d15d52aaab250b18bbc8
manu <manuelsimon2019@gmail.com>
2020-11-10 15:31:34 -0300

    Login avance #2

--- proyectoMMN/app.js
+++ proyectoMMN/app.js
@@ -2,6 +2,7 @@ var createError = require('http-errors');
 var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
+var session = require('express-session');
 var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
@@ -13,6 +14,11 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
+app.use(session(
+  {secret: 'proyecto',
+   resave: false,
+   saveUninitialized: true } 
+  ));
 app.use(express.static(path.join(__dirname, 'public')));
 
 //Aca van nuestras rutas//
--- proyectoMMN/controllers/controladorLogin.js
+++ proyectoMMN/controllers/controladorLogin.js
@@ -1,5 +1,5 @@
 const bcrypt = require ('bcryptjs');
-const db = require('../database/models');
+const db = require('../db/models');
 const users = db.User;
 
 const op = db.Sequelize.Op;
@@ -9,7 +9,25 @@ let controladorLogin = {
         return res.render('login');
         },
 
+
         login: function(req, res){
+            users.findOne(
+                where: [{ email: req.body.email }]
+             )
+            .then( function (user) {
+                if(user == null) {
+                    retun res.send("email incorrecto");
+
+                } else if (bcrypt.compareSync(req.body.password, user.password == false)) {
+                    return res.send("contraseña incorrecta")
+                } else if (bcrypt.compareSync(req.body.password, user.password) {
+                    
+                    req.session.user = user
+                    return res.redirect('/home')
+                }
+
+            })
+            .catch( e => console.log(e))
 
 
         },
