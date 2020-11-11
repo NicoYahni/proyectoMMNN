bb060d12a1327abf17517e0822e1852dee756ce4
Marcos <hoggm@udesa.edu.ar>
2020-11-11 12:05:25 -0300

    Busqueda controlador

--- proyectoMMN/controllers/controladorResultadoBusqueda.js
+++ proyectoMMN/controllers/controladorResultadoBusqueda.js
@@ -1,7 +1,28 @@
+let db = require('../db/models/index')
+let op = db.Sequelize.Op;
 let rutaResultadoBusqueda = {
-
+//<%= usuarios[i].Nombre_Usuario%>
     resultadoBusqueda: function(req, res) {
-        res.render("resultadoBusqueda", )   
+        let usuarioBuscado = req.query.usuarioBuscado
+        db.Usuario.findAll(
+            {
+            where: [
+           { Nombre_Usuario : {[op.like]: '%' + usuarioBuscado + '%'}}
+           ] 
+        }
+           
+
+        )
+        .then(function(usuarios){
+            console.log(usuarios)
+
+    
+        res.render('resultadoBusqueda', {usuarios: usuarios})
+        })
+
+        
+        
+          
     }
 }
 
