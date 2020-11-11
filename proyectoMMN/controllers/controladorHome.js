1472dfdfb090c15c591b3de59447d875a4548753
Marcos <hoggm@udesa.edu.ar>
2020-11-11 15:39:58 -0300

    home posteos

--- proyectoMMN/controllers/controladorHome.js
+++ proyectoMMN/controllers/controladorHome.js
@@ -1,9 +1,27 @@
-let rutaHome = {
+let db = require('../db/models/index')
+let controladorHome = {
 
-    home: function(req, res) {
-        res.render("home", )   
+    home: function(req,res) {
+        db.Post.findAll()
+        .then(function(posteos) {
+            console.log(posteos)
+        //res.render('home', {posteos: posteos})
+        })
+        
+
+          
+    },
+    detalle: function(req,res) {
+        //db.Post.findAll()
+        //.then(function(posteos) {
+          //  console.log(posteos)
+        //res.render('home', {posteos: posteos})
+        //})
+        
+
+          
     }
 }
 
 
-module.exports = rutaHome
+module.exports = controladorHome
