97862d70a38fdd6cd70498893ba99eddf96fb4b8
Marcos <hoggm@udesa.edu.ar>
2020-11-11 12:25:40 -0300

    INCLUDE header se busca desde cualq pag

--- proyectoMMN/controllers/controladorResultadoBusqueda.js
+++ proyectoMMN/controllers/controladorResultadoBusqueda.js
@@ -1,7 +1,7 @@
 let db = require('../db/models/index')
 let op = db.Sequelize.Op;
 let rutaResultadoBusqueda = {
-//<%= usuarios[i].Nombre_Usuario%>
+
     resultadoBusqueda: function(req, res) {
         let usuarioBuscado = req.query.usuarioBuscado
         db.Usuario.findAll(
