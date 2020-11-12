fe1dd2c31c4816dc58ffda86a221f58d8367b354
Marcos <hoggm@udesa.edu.ar>
2020-11-12 00:42:04 -0300

    informacion de BdD en detallUsuario depende del ID

--- proyectoMMN/controllers/controladorDetalleUsuario.js
+++ proyectoMMN/controllers/controladorDetalleUsuario.js
@@ -1,10 +1,18 @@
+const db = require("../db/models/index")
 let controladorDetalleUsuario = {
 
     detalleUsuario: function(req, res) {
         let idUsuario = req.params.id
         // atento porque en un momento no me funcionaba y dsp me funciono de la nada. no se bien que toque 
         // pero ahora te deriva al detalle de usuario indicado desde el home :)
-        res.render("detalleUsuario", {idUsuario: idUsuario})   
+
+        // aca hay que hacer un findByPk si no me equivoco usando el idUsuario traido con req.params
+        // y con eso mandar a la vista toda la info de ese usuario
+        db.Usuario.findByPk(idUsuario)
+        .then (function(usuario){
+            res.render("detalleUsuario", {usuario: usuario}) 
+        })
+          
     }
 }
 
