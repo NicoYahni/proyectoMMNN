2c5a6afb73c68a8c1b3af3a9b98be0ea348a6cd9
Marcos <hoggm@udesa.edu.ar>
2020-11-12 00:27:25 -0300

    ir al detalleUsuario desde Home Funciona

--- proyectoMMN/controllers/controladorDetalleUsuario.js
+++ proyectoMMN/controllers/controladorDetalleUsuario.js
@@ -2,7 +2,8 @@ let controladorDetalleUsuario = {
 
     detalleUsuario: function(req, res) {
         let idUsuario = req.params.id
-
+        // atento porque en un momento no me funcionaba y dsp me funciono de la nada. no se bien que toque 
+        // pero ahora te deriva al detalle de usuario indicado desde el home :)
         res.render("detalleUsuario", {idUsuario: idUsuario})   
     }
 }
