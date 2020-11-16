484bb90d641323c97d20ea4584133997a9001417
Marcos <hoggm@udesa.edu.ar>
2020-11-16 10:44:03 -0300

    a

--- proyectoMMN/routes/registracion.js
+++ proyectoMMN/routes/registracion.js
@@ -2,11 +2,6 @@ var express = require('express');
 var router = express.Router();
 var controladorRegistracion = require ("../controllers/controladorRegistracion");
 
-
-
-router.get ("/registracion", controladorRegistracion.registracion)
-router.post ("/registracion", controladorRegistracion.storeUser)
-
 router.get ("/", controladorRegistracion.registracion)
 router.post ("/", controladorRegistracion.storeUser)
 
