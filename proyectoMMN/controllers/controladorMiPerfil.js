88a275d65f8fd1a9e5515f3cbc521f38192d852a
Marcos <hoggm@udesa.edu.ar>
2020-11-16 20:42:56 -0300

    mi perfil muestra posteos propios y cantidad posts

--- proyectoMMN/controllers/controladorMiPerfil.js
+++ proyectoMMN/controllers/controladorMiPerfil.js
@@ -1,19 +1,47 @@
 const db = require("../db/models/index")
 let controladorMiPerfil = {
 
-    miPerfil: function(req, res) {
-       // if (req.session.user == undefined){
-         //   res.redirect("/login");
-           // }
+//     miPerfil: function(req, res) {
+//        if (req.session.user == undefined){
+//            res.redirect("/login");
+//            }
             
-        let idUsuario = req.session.user.idUsuario
+//         let idUsuario = req.session.user.idUsuario
 
-        db.Usuario.findByPk(idUsuario)
-        .then (function(usuario){
+//         db.Usuario.findByPk(idUsuario)
+//         .then (function(usuario){
+//           console.log('sarasa');
+//         res.render("miPerfil", {usuario: usuario, usuarioLogueado : req.session.user })   
+//         //me doy cuenta que todo esto de miPerfil no hacia falta porque podria traer la informacion del usuario desde 
+//         // la vista directamente usando user.yAcaLaColumnaQueQuiera
+//         // lo saco por las dudas me rompa
+//     })
+// },
+
+        informacion: function(req, res) {
+          console.log('LLEGUE O NO LLEGUE');
+          let idUsuario = req.session.user.idUsuario
+          db.Post.findAll(
+            {
+            where:  {
+                   
+              Id_usuario : idUsuario
+           
+             
+               }
+              
+         }
             
-        res.render("miPerfil", {usuario: usuario, usuarioLogueado : req.session.user })   
-    })
-}
+   
+         )
+          .then (function(posteos){
+            console.log('sarasa2');
+           console.log(posteos)
+          res.render("miPerfil", {posteos: posteos, usuarioLogueado : req.session.user })   
+          console.log('lo mandoooooo')
+        })
+        }
+
 }
 
 
--- proyectoMMN/routes/miPerfil.js
+++ proyectoMMN/routes/miPerfil.js
@@ -2,8 +2,8 @@ var express = require('express');
 var router = express.Router();
 var controladorMiPerfil = require ("../controllers/controladorMiPerfil");
 
-router.get ("/", controladorMiPerfil.miPerfil)
-
+//router.get ("/", controladorMiPerfil.miPerfil)
+router.get ("/", controladorMiPerfil.informacion)
 
 
 module.exports = router;
