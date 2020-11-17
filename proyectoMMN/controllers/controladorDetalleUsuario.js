b9a062761b59259e091a4578a888ebff65274c69
Marcos <hoggm@udesa.edu.ar>
2020-11-16 23:12:08 -0300

    una lastima no pude llegar a resolver lo de

--- proyectoMMN/controllers/controladorDetalleUsuario.js
+++ proyectoMMN/controllers/controladorDetalleUsuario.js
@@ -1,14 +1,41 @@
 const db = require("../db/models/index")
 let controladorDetalleUsuario = {
 
-    detalleUsuario: function(req, res) {
+   
+    postUsuario: function(req, res) {
+        console.log('LLEGUE O NO LLEGUE');
         let idUsuario = req.params.id
-        db.Usuario.findByPk(idUsuario)
-        .then (function(usuario){
-            res.render("detalleUsuario", {usuario: usuario, usuarioLogueado : req.session.user}) 
-        })
+        db.Post.findAll(
+          {
+          where:  {
+                 
+            Id_usuario : idUsuario
+         
+           
+             }
+            
+       }
           
-    }
+ 
+       )
+        .then (function(posteos){
+          console.log('sarasa2');
+         console.log(posteos)
+        res.render("detalleUsuario", {posteos: posteos, usuarioLogueado : req.session.user })   
+        console.log('lo mandoooooo')
+      })
+          
+    },
+    detalleUsuario: function(req, res) {
+      console.log('ACA ESTOY');
+      let idUsuario = req.params.id
+      db.Usuario.findByPk(idUsuario)
+      .then (function(usuario){
+          res.render("detalleUsuario", {usuario: usuario, usuarioLogueado : req.session.user}) 
+      })
+        
+  }
+
 }
 
 
